adc2c86c47ed2f163dcad230857d171d220ee1a1
Jash Maniar <101001262+jashmaniar@users.noreply.github.com>
2024-09-01 15:29:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('mousemove', function(e) {
+    const cursor = document.querySelector('.custom-cursor');
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+});
